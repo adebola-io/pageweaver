2409b83ebd862356986cee0163b1fd1b15973ff2
Rael-x <adebolaakomolafe@gmail.com>
2022-01-28 12:37:06 +0100

    work

---
+++ pageweaver.pages/test.page/src/Test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles/--test.css";
+
+const Test = () => {
+  return (
+    <div className="test">
+      test page.
+    </div>
+    )
+}
+  
+export default Test;
---
+++ pageweaver.pages/test.page/src/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Test from "./Test";
+ReactDOM.render(<Test />, document.getElementById("root"));
