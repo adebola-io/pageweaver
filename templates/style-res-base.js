2747960b28e0afddb229e11e0dc6f658d76aa44a
Rael-x <adebolaakomolafe@gmail.com>
2022-02-02 14:46:56 +0100

    Solved: SCSS responsiveness @use error

--- templates/style-res-base.js
+++ templates/style-res-base.js
@@ -8,32 +8,32 @@ module.exports = `\`/*
 /* Extra large screens, 4K Screens & TV */
 \${args.scss ? "@use" : "@import"} '\${
   args.scss ? "_" : ""
-}\${args.jsFramework ? \`\` : \`\${mainParam}.\`}res.2160px\${args.scss ? "" : ".css"}';
+}\${args.jsFramework ? \`\` : \`\${mainParam}\${args.scss ? '_': '.'}\`}res\${args.scss ? '_': '.'}2160px\${args.scss ? "" : ".css"}';
 
 /* Laptop & iPad screens */
 \${args.scss ? "@use" : "@import"} '\${
   args.scss ? "_" : ""
-}\${args.jsFramework ? \`\` : \`\${mainParam}.\`}res.1440px\${args.scss ? "" : ".css"}';
+}\${args.jsFramework ? \`\` : \`\${mainParam}\${args.scss ? '_': '.'}\`}res\${args.scss ? '_': '.'}1440px\${args.scss ? "" : ".css"}';
 \${args.scss ? "@use" : "@import"} '\${
   args.scss ? "_" : ""
-}\${args.jsFramework ? \`\` : \`\${mainParam}.\`}res.1024px\${args.scss ? "" : ".css"}';
+}\${args.jsFramework ? \`\` : \`\${mainParam}\${args.scss ? '_': '.'}\`}res\${args.scss ? '_': '.'}1024px\${args.scss ? "" : ".css"}';
 
 /* Tablet Screens */
 \${args.scss ? "@use" : "@import"} '\${
   args.scss ? "_" : ""
-}\${args.jsFramework ? \`\` : \`\${mainParam}.\`}res.768px\${args.scss ? "" : ".css"}';
+}\${args.jsFramework ? \`\` : \`\${mainParam}\${args.scss ? '_': '.'}\`}res\${args.scss ? '_': '.'}768px\${args.scss ? "" : ".css"}';
 
 /* Large Mobile devices */
 \${args.scss ? "@use" : "@import"} '\${
   args.scss ? "_" : ""
-}\${args.jsFramework ? \`\` : \`\${mainParam}.\`}res.425px\${args.scss ? "" : ".css"}';
+}\${args.jsFramework ? \`\` : \`\${mainParam}\${args.scss ? '_': '.'}\`}res\${args.scss ? '_': '.'}425px\${args.scss ? "" : ".css"}';
 
 /* Medium Mobile devices */
 \${args.scss ? "@use" : "@import"} '\${
   args.scss ? "_" : ""
-}\${args.jsFramework ? \`\` : \`\${mainParam}.\`}res.375px\${args.scss ? "" : ".css"}';
+}\${args.jsFramework ? \`\` : \`\${mainParam}\${args.scss ? '_': '.'}\`}res\${args.scss ? '_': '.'}375px\${args.scss ? "" : ".css"}';
 
 /* Small Mobile devices */
 \${args.scss ? "@use" : "@import"} '\${
   args.scss ? "_" : ""
-}\${args.jsFramework ? \`\` : \`\${mainParam}.\`}res.320px\${args.scss ? "" : ".css"}';\``;
+}\${args.jsFramework ? \`\` : \`\${mainParam}\${args.scss ? '_': '.'}\`}res\${args.scss ? '_': '.'}320px\${args.scss ? "" : ".css"}';\``;
